a2ff5d51cdfa28010bf4678bf681dd6cde39fddf
nese <nesekrmn02@gmail.com>
2023-10-07 00:19:01 +0300

    while dongu added

---
+++ dongu7/doWhile.js
@@ -0,0 +1,7 @@
+var s=1 ,top=0;
+do
+{
+    top=top+s;
+    s++;
+}while(s<=5);
+document.write("toplam="+top);
--- dongu7/while.js
+++ dongu7/while.js
@@ -1,9 +1,9 @@
 var sayac =0;
 
 while(sayac<=5)
-{   var top=0;
+{  // var top=0;
      document.write("Merhaba");
-     top=top+sayac;
-     document.write(top);
+     //top=top+sayac;
+     //document.write(top);
      sayac++;
 }
